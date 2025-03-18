29a8da46888b5f511f5fbb29c3129641d0ce313d
arnnvv <arnav.900@protonmail.com>
2025-03-18 23:50:33 +0530

    btr

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,9 +3,11 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import type { JSX } from "react";
-import { sendEmailAtn } from "@/app/actions";
+import { getCurrentSession, sendEmailAtn } from "@/app/actions";
 
-export default function ContactForm(): JSX.Element {
+export default async function ContactForm(): Promise<JSX.Element> {
+  const { session, user } = await getCurrentSession();
+
   return (
     <main className="bg-gray-100 dark:bg-zinc-950 min-h-screen py-12 px-4">
       <div className="max-w-md mx-auto">
@@ -18,6 +20,8 @@ export default function ContactForm(): JSX.Element {
                 placeholder="Your Email"
                 className="border-gray-200 dark:border-zinc-800 bg-white/80 dark:bg-zinc-900/90 focus:border-blue-500 dark:focus:border-blue-400 focus:ring-blue-500 dark:focus:ring-blue-400 rounded-lg text-gray-500 dark:text-zinc-200"
                 required
+                defaultValue={session ? user.email : ""}
+                readOnly={!!session}
               />
             </div>
 
