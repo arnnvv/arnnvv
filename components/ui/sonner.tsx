ab9dcef72fd2bcc62abddcf89703bb4a3263d5fb
arnnvv <arnav.900@protonmail.com>
2025-03-26 15:45:58 +0530

    nb

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import type { JSX } from "react";
+import type { CSSProperties, JSX } from "react";
 import type { ToasterProps } from "sonner";
 import { Toaster as Sonner } from "sonner";
 
@@ -17,7 +17,7 @@ export const Toaster = ({ ...props }: ToasterProps): JSX.Element => {
           "--normal-bg": "var(--popover)",
           "--normal-text": "var(--popover-foreground)",
           "--normal-border": "var(--border)",
-        } as React.CSSProperties
+        } as CSSProperties
       }
       {...props}
     />
