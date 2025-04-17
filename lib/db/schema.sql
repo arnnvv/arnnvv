4b3366358ff94c138b00dd4bdf162c7b784a4e55
arnnvv <arnav.900@protonmail.com>
2025-04-17 05:46:19 +0530

    sql

--- lib/db/schema.sql
+++ lib/db/schema.sql
@@ -1,20 +1,29 @@
 CREATE TABLE "arnnvv_sessions" (
-	"id" text PRIMARY KEY NOT NULL,
-	"user_id" integer NOT NULL,
-	"expires_at" timestamp with time zone NOT NULL
+    "id"         text PRIMARY KEY NOT NULL,
+    "user_id"    integer NOT NULL,
+    "expires_at" timestamp with time zone NOT NULL
 );
---> statement-breakpoint
+
 CREATE TABLE "arnnvv_users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"google_id" text NOT NULL,
-	"email" varchar NOT NULL,
-	"name" text NOT NULL,
-	"picture" text NOT NULL,
-	CONSTRAINT "arnnvv_users_google_id_unique" UNIQUE("google_id"),
-	CONSTRAINT "arnnvv_users_email_unique" UNIQUE("email")
+    "id"         serial PRIMARY KEY NOT NULL,
+    "google_id"  text NOT NULL,
+    "email"      varchar NOT NULL,
+    "name"       text NOT NULL,
+    "picture"    text NOT NULL,
+    CONSTRAINT   "arnnvv_users_google_id_unique" UNIQUE("google_id"),
+    CONSTRAINT   "arnnvv_users_email_unique"     UNIQUE("email")
 );
---> statement-breakpoint
-ALTER TABLE "arnnvv_sessions" ADD CONSTRAINT "arnnvv_sessions_user_id_arnnvv_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."arnnvv_users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-CREATE INDEX "session_user_id_idx" ON "arnnvv_sessions" USING btree ("user_id");--> statement-breakpoint
-CREATE INDEX "google_id_idx" ON "arnnvv_users" USING btree ("google_id");--> statement-breakpoint
-CREATE INDEX "email_idx" ON "arnnvv_users" USING btree ("email");
+
+ALTER TABLE "arnnvv_sessions"
+ADD CONSTRAINT "arnnvv_sessions_user_id_arnnvv_users_id_fk"
+FOREIGN KEY ("user_id")
+REFERENCES "public"."arnnvv_users"("id")
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+CREATE INDEX "session_user_id_idx"
+ON "arnnvv_sessions" USING btree ("user_id");
+CREATE INDEX "google_id_idx"
+ON "arnnvv_users" USING btree ("google_id");
+CREATE INDEX "email_idx"
+ON "arnnvv_users" USING btree ("email");
