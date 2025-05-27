c357130aaf977d344236f5cbb1c96f7a464aaf68
arnnvv <arnav.900@protonmail.com>
2025-05-27 14:27:11 +0530

    try

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -51,6 +51,17 @@ export async function generateMetadata({
   return {
     title: `${post.title} | Arnav Sharma`,
     description: excerpt,
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
     openGraph: {
       title: post.title,
       description: excerpt,
@@ -61,25 +72,25 @@ export async function generateMetadata({
       images: [
         {
           url: imageUrl,
-          width: 1200,
-          height: 630,
           alt: post.title,
           type: "image/jpeg",
         },
       ],
-      siteName: "Arnav Sharma's Blog",
+      siteName: "arnnvv",
     },
     twitter: {
-      card: "summary_large_image",
       title: post.title,
       description: excerpt,
-      images: [imageUrl],
-      creator: "@arnnvv",
-      site: "@arnnvv",
-    },
-    other: {
-      "twitter:image": imageUrl,
-      "twitter:image:alt": post.title,
+      images: [
+        {
+          url: imageUrl,
+          alt: post.title,
+          type: "image/jpeg",
+        },
+      ],
+      creator: "@arnnnvvv",
+      creatorId: "@arnnnvvv",
+      site: "@arnnnvvv",
     },
   };
 }
